ba6d6875e69f55d55eb2a0f74cb4531a28e3abb9
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2026-06-09 18:30:03 -0500

    new commit

--- venture-os/agent-engine/api/tasks/task_runner.py
+++ venture-os/agent-engine/api/tasks/task_runner.py
@@ -5,5 +5,18 @@ from core.orchestrator import Orchestrator
 
 @shared_task(bind=True,max_retries =3 )
 def queueing(task_id:str , user_input:str, llm):
-    app.
-    
+    if app:
+        celery_id = app.oid
+        print(f"Task {task_id} is being processed by Celery worker with ID: {celery_id}")
+        try:
+            response  = (engine.table("tasks").insert({"celery_id": celery_id}).execute())
+            
+            if response== None:
+                print(f"Failed to update task {task_id} with Celery ID: {response.text}")
+            else:
+                updated_response = engine.table("tasks").update({"celery_id": celery_id, "status": "processing"}).eq("id", task_id).execute()
+                if updated_response != None:
+                    orchastrator = Orchestrator(llm=llm)
+                    orchastrator.process_user_request(user_input)
+        except Exception as e:
+            print(f"Failed to update task {task_id} status: {e}")
