f7e1b03a1edfcb9db87f2acd4dcaacec12b222de
Hitesh Bhasin <bhasinsukh@gmail.com>
2026-06-20 12:41:43 -0500

    new commit

--- venture-os/agent-engine/scripts/taskQueue.py
+++ venture-os/agent-engine/scripts/taskQueue.py
@@ -1,5 +1,11 @@
 from typing import Any
 from collections import deque
+from supabase import create_client, Client
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+# Load from root .env (VentureOS/.env)
+load_dotenv(dotenv_path=Path(__file__).resolve().parents[3] / ".env")
 
 class TaskQueue:
     def __init__(self):
@@ -10,8 +16,19 @@ class TaskQueue:
         self._list.append(element)
         self._add_to_db(element)
 
-    def _add_to_db(self,element:Any) ->None:  
-        pass # @need to add the DB logic to add the task to the DB
+    def _add_to_db(self,element:dict) ->None:  
+        SUPABASE_URL = os.getenv("SUPABASE_URL")
+        SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY") or os.getenv("SUPABASE_KEY")
+
+        if not SUPABASE_URL or not SUPABASE_KEY:
+            raise EnvironmentError(
+        "SUPABASE_URL and SUPABASE_KEY must be set in your .env file.")
+
+        try :
+            engine : Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+            engine.table("tasks")
+            
+        except:
     
     def highest_priority(self)->Any:
         return self._list.popleft()
